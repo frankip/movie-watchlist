3a1d5ce733c24ca516af01a78dcb4d8529a8d1a9
Francky <gwadafrank@gmail.com>
2020-06-30 21:27:04 +0300

    feat - make api calls requests

--- app/views.py
+++ app/views.py
@@ -1,13 +1,18 @@
 from flask import render_template
 from app import app 
 
+from .request import get_movies
+
 # views
 @app.route('/')
 def index():
     """
     view root page that returns index page
     """
-    return render_template('index.html')
+    popular_movies = get_movies('popular')
+    # print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title=title,popular = popular_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
