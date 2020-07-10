4bc7f8d6661d9729e6d2ad722d445855f5c3d940
Francky <gwadafrank@gmail.com>
2020-07-10 16:59:58 +0300

    update config env

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Confifg:
     common confugurations
     """
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    # MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://francky:123bigman@localhost/movielist'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     # simple mde  configurations
